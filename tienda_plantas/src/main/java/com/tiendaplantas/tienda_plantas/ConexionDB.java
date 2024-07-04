25b89ae01602614920adb8ea712e9afecc19b389
dafhome <d.fernalvarez@gmail.com>
2024-07-04 21:00:01 +0200

    commit

--- tienda_plantas/src/main/java/com/tiendaplantas/tienda_plantas/ConexionDB.java
+++ tienda_plantas/src/main/java/com/tiendaplantas/tienda_plantas/ConexionDB.java
@@ -2,8 +2,6 @@ package com.tiendaplantas.tienda_plantas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class ConexionDB {
   public static Connection CreateDB() {
@@ -12,10 +10,6 @@ public class ConexionDB {
       Class.forName("com.mysql.cj.jdbc.Driver");
       conexion1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda_plantas", "root",
           "Andy5100");
-      // System.out.println("Conexión realizada correctamente");
-
-
-
     } catch (Exception e) {
       System.out.println("no funciona");
     }
